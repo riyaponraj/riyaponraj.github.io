1b4d4f6ba35313541441f910ff92a42f62e98d9c
Riya Ponraj <81085863+riyaponraj@users.noreply.github.com>
2024-10-18 23:43:58 +0530

    Update Cell.js

--- src/components/Projects/Cell.js
+++ src/components/Projects/Cell.js
@@ -7,7 +7,7 @@ const Cell = ({ data }) => (
     <article className="mini-post">
       <header>
         <h3><a href={data.link}>{data.title}</a></h3>
-        <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
+        // <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
       </header>
       <a href={data.link} className="image">
         <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} />
@@ -24,7 +24,7 @@ Cell.propTypes = {
     title: PropTypes.string.isRequired,
     link: PropTypes.string,
     image: PropTypes.string.isRequired,
-    date: PropTypes.string.isRequired,
+    // date: PropTypes.string.isRequired,
     desc: PropTypes.string.isRequired,
   }).isRequired,
 };
